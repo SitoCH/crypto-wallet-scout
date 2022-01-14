8f9aa34687132e8d16485ad7d4a29988a85295dd
Simone Grignola <sito@grignola.ch>
2022-01-14 23:45:07 +0100

    Another fix for page refresh on native images

--- src/main/java/ch/grignola/web/EndpointNotFoundExceptionMapper.java
+++ src/main/java/ch/grignola/web/EndpointNotFoundExceptionMapper.java
@@ -4,37 +4,26 @@ import io.quarkus.arc.Priority;
 
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Priorities;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URI;
-import java.nio.charset.StandardCharsets;
+
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 
 @Provider
 @Priority(Priorities.USER)
 public class EndpointNotFoundExceptionMapper implements ExceptionMapper<NotFoundException> {
 
     @Override
-    public Response toResponse(NotFoundException e) {
-        final InputStream indexIS = Thread.currentThread().getContextClassLoader().getResourceAsStream("index.html");
-        final InputStreamReader isr = new InputStreamReader(indexIS, StandardCharsets.UTF_8);
-        final StringBuilder sb = new StringBuilder();
-        int c;
-        try {
-            while ((c = isr.read()) != -1) {
-                sb.append((char) c);
-            }
-        } catch (IOException ex) {
-            return Response.temporaryRedirect(URI.create("/")).build();
-        }
-
-        return Response.ok(sb.toString(), MediaType.TEXT_HTML_TYPE)
-                .cacheControl(CacheControl.valueOf("no-cache, no-store, must-revalidate"))
-                .build();
+    @Produces(MediaType.TEXT_HTML)
+    public Response toResponse(NotFoundException exception) {
+        InputStream resource = ClassLoader.getSystemResourceAsStream("META-INF/resources/index.html");
+        return null == resource ? Response.status(NOT_FOUND).build() : Response.ok()
+                .entity(resource)
+                .cacheControl(CacheControl.valueOf("no-cache, no-store, must-revalidate")).build();
     }
 }
