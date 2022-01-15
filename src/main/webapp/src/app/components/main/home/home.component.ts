cdbf456b400104b88553f0e31c61072c508eb63b
Simone Grignola <sito@grignola.ch>
2022-01-15 22:08:22 +0100

    Disable auto redirect on home

--- src/main/webapp/src/app/components/main/home/home.component.ts
+++ src/main/webapp/src/app/components/main/home/home.component.ts
@@ -1,27 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Select } from "@ngxs/store";
-import { AuthenticationState } from "../../../state/authentication.state";
-import { Observable } from "rxjs";
-import { Router } from "@angular/router";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  @Select(AuthenticationState.isAuthenticated) isAuthenticated$: Observable<boolean> | undefined;
-
-  constructor(private router: Router) {
-  }
-
-  ngOnInit(): void {
-    this.isAuthenticated$?.subscribe(isAuthenticated => {
-      if (isAuthenticated) {
-        this.router.navigate(['/dashboard']);
-      }
-    });
-  }
 
 }
