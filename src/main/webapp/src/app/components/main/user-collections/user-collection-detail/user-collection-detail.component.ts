68cb20e7f4e9b9035220d0c976a4f5b36b25c8f4
Simone Grignola <sito@grignola.ch>
2022-01-19 00:51:41 +0100

    Dump all tokens in a collection

--- src/main/webapp/src/app/components/main/user-collections/user-collection-detail/user-collection-detail.component.ts
+++ src/main/webapp/src/app/components/main/user-collections/user-collection-detail/user-collection-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import { UserCollectionService } from "../../../../services/user-collection.service";
-import { AddressBalance } from "../../../../../generated/client";
+import { AddressBalance, TokenBalance } from "../../../../../generated/client";
 
 @Component({
   selector: 'app-user-collection-detail',
@@ -12,6 +12,7 @@ export class UserCollectionDetailComponent implements OnInit {
 
   id!: number;
   addressBalances: AddressBalance[] | null = null;
+  aggregatedTokenBalances: TokenBalance[] | null = null;
   collectionUsdValue: number | null = null;
 
   constructor(private userCollectionService: UserCollectionService,
@@ -30,15 +31,26 @@ export class UserCollectionDetailComponent implements OnInit {
   private loadBalance() {
     this.addressBalances = null;
     this.collectionUsdValue = null;
+    this.aggregatedTokenBalances = null;
     this.userCollectionService.getAddressBalance(this.id)
       .then(data => {
         this.addressBalances = data;
         let collectionUsdValue = 0;
+        let tokens = new Map<string, TokenBalance>();
         data.forEach(addressBalance => {
           addressBalance.tokenBalances.forEach(tokenBalance => {
             collectionUsdValue += tokenBalance.usdValue;
+            if (!tokens.has(tokenBalance.tokenId)) {
+              tokens.set(tokenBalance.tokenId, tokenBalance);
+            } else {
+              // @ts-ignore
+              tokens[tokenBalance.tokenId].nativeValue += tokenBalance.nativeValue;
+              // @ts-ignore
+              tokens[tokenBalance.tokenId].usdValue += tokenBalance.usdValue;
+            }
           });
         });
+        this.aggregatedTokenBalances = Array.from(tokens, ([name, value]) => (value));
         this.collectionUsdValue = collectionUsdValue;
       })
   }
