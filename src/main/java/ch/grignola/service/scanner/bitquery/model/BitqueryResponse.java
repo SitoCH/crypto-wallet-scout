fe9552398de84131bec676cb85687065d2197f68
Simone Grignola <sito@grignola.ch>
2022-01-21 22:24:45 +0100

    Added @JsonProperty to BitqueryResponse models

--- src/main/java/ch/grignola/service/scanner/bitquery/model/BitqueryResponse.java
+++ src/main/java/ch/grignola/service/scanner/bitquery/model/BitqueryResponse.java
@@ -1,10 +1,10 @@
 package ch.grignola.service.scanner.bitquery.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @RegisterForReflection
 public class BitqueryResponse {
-
+    @JsonProperty("data")
     public Data data;
-
 }
--- src/main/java/ch/grignola/service/scanner/bitquery/model/Currency.java
+++ src/main/java/ch/grignola/service/scanner/bitquery/model/Currency.java
@@ -1,9 +1,12 @@
 package ch.grignola.service.scanner.bitquery.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Currency {
-
+    @JsonProperty("address")
     public String address;
+    @JsonProperty("symbol")
     public String symbol;
+    @JsonProperty("tokenType")
     public String tokenType;
-
 }
