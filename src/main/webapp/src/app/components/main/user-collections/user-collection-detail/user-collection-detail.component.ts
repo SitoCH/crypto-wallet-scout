4797cf77d8ac6a79434c6ca16f8c786f70509c5f
Simone Grignola <sito@grignola.ch>
2022-01-22 22:30:45 +0100

    Small changes to the UI

--- src/main/webapp/src/app/components/main/user-collections/user-collection-detail/user-collection-detail.component.ts
+++ src/main/webapp/src/app/components/main/user-collections/user-collection-detail/user-collection-detail.component.ts
@@ -15,7 +15,6 @@ import { TokenList } from "../../../../utils/balance";
 export class UserCollectionDetailComponent implements OnInit {
 
   id!: number;
-  addressBalances: AddressBalance[] | null = null;
   aggregatedTokenBalances: TokenList | null = null;
 
   constructor(private userCollectionService: UserCollectionService,
@@ -33,11 +32,9 @@ export class UserCollectionDetailComponent implements OnInit {
   }
 
   private loadBalance() {
-    this.addressBalances = null;
     this.aggregatedTokenBalances = null;
     this.userCollectionService.getAddressBalance(this.id)
       .then(data => {
-        this.addressBalances = data;
         let collectionUsdValue = 0;
         let tokens = new Map<string, TokenBalance>();
         data.forEach(addressBalance => {
