cd3345183735888c39bad7a5da4b8c591281b03e
Simone Grignola <sito@grignola.ch>
2022-04-12 00:05:33 +0200

    Removed more unused fields from models

--- src/main/java/ch/grignola/service/token/model/MarketData.java
+++ src/main/java/ch/grignola/service/token/model/MarketData.java
@@ -9,27 +9,23 @@ public class MarketData {
     @JsonProperty("current_price")
     public CurrentPrice currentPrice;
     @JsonProperty("ath")
-    public Ath ath;
+    public ValueByCurrency ath;
     @JsonProperty("ath_change_percentage")
-    public AthChangePercentage athChangePercentage;
-    @JsonProperty("ath_date")
-    public AthDate athDate;
+    public ValueByCurrency athChangePercentage;
     @JsonProperty("atl")
-    public Atl atl;
+    public ValueByCurrency atl;
     @JsonProperty("atl_change_percentage")
-    public AtlChangePercentage atlChangePercentage;
-    @JsonProperty("atl_date")
-    public AtlDate atlDate;
+    public ValueByCurrency atlChangePercentage;
     @JsonProperty("market_cap")
-    public MarketCap marketCap;
+    public ValueByCurrency marketCap;
     @JsonProperty("fully_diluted_valuation")
-    public FullyDilutedValuation fullyDilutedValuation;
+    public ValueByCurrency fullyDilutedValuation;
     @JsonProperty("total_volume")
-    public TotalVolume totalVolume;
+    public ValueByCurrency totalVolume;
     @JsonProperty("high_24h")
-    public High24h high24h;
+    public ValueByCurrency high24h;
     @JsonProperty("low_24h")
-    public Low24h low24h;
+    public ValueByCurrency low24h;
     @JsonProperty("price_change_24h")
     public float priceChange24h;
     @JsonProperty("price_change_percentage_24h")
@@ -51,27 +47,27 @@ public class MarketData {
     @JsonProperty("market_cap_change_percentage_24h")
     public float marketCapChangePercentage24h;
     @JsonProperty("price_change_24h_in_currency")
-    public PriceChange24hInCurrency priceChange24hInCurrency;
+    public ValueByCurrency priceChange24hInCurrency;
     @JsonProperty("price_change_percentage_1h_in_currency")
-    public PriceChangePercentageInCurrency priceChangePercentage1hInCurrency;
+    public ValueByCurrency priceChangePercentage1hInCurrency;
     @JsonProperty("price_change_percentage_24h_in_currency")
-    public PriceChangePercentageInCurrency priceChangePercentage24hInCurrency;
+    public ValueByCurrency priceChangePercentage24hInCurrency;
     @JsonProperty("price_change_percentage_7d_in_currency")
-    public PriceChangePercentageInCurrency priceChangePercentage7dInCurrency;
+    public ValueByCurrency priceChangePercentage7dInCurrency;
     @JsonProperty("price_change_percentage_14d_in_currency")
-    public PriceChangePercentageInCurrency priceChangePercentage14dInCurrency;
+    public ValueByCurrency priceChangePercentage14dInCurrency;
     @JsonProperty("price_change_percentage_30d_in_currency")
-    public PriceChangePercentageInCurrency priceChangePercentage30dInCurrency;
+    public ValueByCurrency priceChangePercentage30dInCurrency;
     @JsonProperty("price_change_percentage_60d_in_currency")
-    public PriceChangePercentageInCurrency priceChangePercentage60dInCurrency;
+    public ValueByCurrency priceChangePercentage60dInCurrency;
     @JsonProperty("price_change_percentage_200d_in_currency")
-    public PriceChangePercentageInCurrency priceChangePercentage200dInCurrency;
+    public ValueByCurrency priceChangePercentage200dInCurrency;
     @JsonProperty("price_change_percentage_1y_in_currency")
-    public PriceChangePercentageInCurrency priceChangePercentage1yInCurrency;
+    public ValueByCurrency priceChangePercentage1yInCurrency;
     @JsonProperty("market_cap_change_24h_in_currency")
-    public MarketCapChange24hInCurrency marketCapChange24hInCurrency;
+    public ValueByCurrency marketCapChange24hInCurrency;
     @JsonProperty("market_cap_change_percentage_24h_in_currency")
-    public MarketCapChangePercentage24hInCurrency marketCapChangePercentage24hInCurrency;
+    public ValueByCurrency marketCapChangePercentage24hInCurrency;
     @JsonProperty("circulating_supply")
     public float circulatingSupply;
     @JsonProperty("last_updated")
